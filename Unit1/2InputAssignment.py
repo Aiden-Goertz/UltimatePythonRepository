340d8f994f937d8176463ce442d52049cf9b572a
Aiden <26aidengoertz@usd266.com>
2024-03-28 12:58:07 -0500

    assignment 2

--- Unit1/2InputAssignment.py
+++ Unit1/2InputAssignment.py
@@ -1,8 +1,22 @@
 print("########## 1.2.1 ##########")
+name = input("What is your name? ")
+print(name)
+print(name)
 
 print("########## 1.2.2 ##########")
+name = input("What is your name? ")
+print("!" + name + "!" + name + "!")
 
 print("########## 1.2.3 ##########")
+firstname = input("Enter your first name: ")
+lastname = input("Enter your last name: ")
+street = input("Enter yout street address: ")
+city = input("Enter your address city: ")
+state = input("Enter your address state: ")
+zip = input("Enter your zip code: ")
+print(firstname + " " + lastname)
+print(street)
+print(city + ", " + state + " " + zip)
 
 print("########## 1.2.4 ##########")
 
@@ -10,6 +24,9 @@ print("########## 1.2.4 ##########")
 part = input("The 1st part: ")
 part = input("The 1st part: ")
 part = input("The 1st part: ")
-print(part + part + part)
+print(part + "-" + part + "-" + part + "!")
 
 print("########## 1.2.5 ##########")
+name = input("Please type in a name: ")
+year = input("Please type in a year: ")
+print(name + " is a valiant knight, born in the year " + year + ". One morning " + name + " woke up to an awful racket: a dragon was approaching the village. Only " + name + " could save the village's residents.")
