e09aa767c7f1f10812868ed3e4b7f336c9189aa2
Aiden <26aidengoertz@usd266.com>
2024-04-17 12:04:14 -0500

    3.1

--- Unit3/1WhileLoopsAssignment.py
+++ Unit3/1WhileLoopsAssignment.py
@@ -1,21 +1,58 @@
 # ========== 3.1.1 ==========
 
+# number = 0
+# while number < 30:
+#     number = number + 2
+#     print(number)
 
 # ========== 3.1.2 ==========
 
 # print("Are you ready?")
 # number = int(input("Please type in a number: "))
-# while number = 0:
-# print(number)
+# while number > 1:
+#     number = number - 1
+#     print(number)
 # print("Go!")
 
 # ========== 3.1.3 ==========
 
+# number = int(input("Upper limit: "))
+# num = 0
+# while number > 0:
+#     num = num + 1
+#     if num < number:
+#         print(num)
+
 
 # ========== 3.1.4 ==========
 
+# number = int(input("Upper limit: "))
+# num = 1
+# while number > 0:
+#     num = num * 2
+#     if num < number:
+#         print(num)
 
 # ========== 3.1.5 ==========
 
+# number = int(input("Upper Limit: "))
+# base = int(input("Base: "))
+# num = 1
+# print(num)
+# while number > 0:
+#     num = num * base
+#     if num <= number:
+#         print(num)
 
 # ========== 3.1.6 ==========
+
+# number = int(input("Limit: "))
+# num = 0
+# numb = 0
+# while number > num:
+#     if number > num:
+#         numb = numb + 1
+#         num = num + numb
+#     else:
+#         break
+# print(num)
