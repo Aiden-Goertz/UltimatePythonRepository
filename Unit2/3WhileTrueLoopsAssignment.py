ef04a41c7dcf9bcd179b018fc6e1b478cab10e9b
Aiden <26aidengoertz@usd266.com>
2024-04-19 11:18:29 -0500

    3.1

--- Unit2/3WhileTrueLoopsAssignment.py
+++ Unit2/3WhileTrueLoopsAssignment.py
@@ -1,103 +1,103 @@
 # ========== 2.3.1 ==========
 
-# input()
-# while True:
-#     word = input("Shall we continue? ")
+input()
+while True:
+    word = input("Shall we continue? ")
    
-#     if word == "no":
-#         print("okay then")
-#         break
-#     print("hi")
+    if word == "no":
+        print("okay then")
+        break
+    print("hi")
 
 # ========== 2.3.2 ==========
 
-# from math import sqrt
-# while True:
-#     num = int(input("please type in a number: "))
-#     if num < 0:
-#         print("invald number")
-#     elif num == 0:
-#         print("Exiting")
-#         break
-#     else:
-#         print(sqrt(num))
+from math import sqrt
+while True:
+    num = int(input("please type in a number: "))
+    if num < 0:
+        print("invald number")
+    elif num == 0:
+        print("Exiting")
+        break
+    else:
+        print(sqrt(num))
 
 # ========== 2.3.3 ==========
 
-# number = 5
-# print("Countdown!")
-# while True:
-#   print(number)
-#   number = number - 1
-#   if number == 0:
-#     break
+number = 5
+print("Countdown!")
+while True:
+  print(number)
+  number = number - 1
+  if number == 0:
+    break
 
-# print("Now!")
+print("Now!")
 
 # ========== 2.3.4 ==========
 
-# password = input("Password: ")
-# while True:
-#     word = (input("Repeat Password: "))
-#     if password == word:
-#         print("User account created!")
-#         break
-#     else:
-#         print("They do not match")
+password = input("Password: ")
+while True:
+    word = (input("Repeat Password: "))
+    if password == word:
+        print("User account created!")
+        break
+    else:
+        print("They do not match")
 
 # ========== 2.3.5 ==========
 
-# atp = 0
+atp = 0
 
-# while True:
-#     pin = int(4321)
-#     PIN = int(input("PIN: "))
-#     atp = atp + 1    
-#     if PIN == pin and atp == 1:
-#         print("Correct! It took you a single attempt!")
-#         break
-#     elif PIN == pin:
-#         print("Correct! It took you", atp, "attempts!")
-#         break
-#     else:
-#         print("Wrong")
+while True:
+    pin = int(4321)
+    PIN = int(input("PIN: "))
+    atp = atp + 1    
+    if PIN == pin and atp == 1:
+        print("Correct! It took you a single attempt!")
+        break
+    elif PIN == pin:
+        print("Correct! It took you", atp, "attempts!")
+        break
+    else:
+        print("Wrong")
 
 # ========== 2.3.6 ==========
 
-# year = int(input("Year: "))
-# leap = year + 1
-# while True:
-#     if leap % 4 == 0:
-#         print("The next leap year after", year, "is", leap)
-#         break
-#     else:
-#         leap = leap + 1
+year = int(input("Year: "))
+leap = year + 1
+while True:
+    if leap % 4 == 0:
+        print("The next leap year after", year, "is", leap)
+        break
+    else:
+        leap = leap + 1
 
 # ========== 2.3.7 ==========
 
-# word = input("Please type in a word: ")
-# sent = word + " "
-# while True:
-#     word = input("Please type in a word: ")
-#     if word == "end":
-#         print(sent)
-#         break
-#     else:
-#        sent = sent + word + " " 
+word = input("Please type in a word: ")
+sent = word + " "
+while True:
+    word = input("Please type in a word: ")
+    if word == "end":
+        print(sent)
+        break
+    else:
+       sent = sent + word + " " 
 
 # ========== 2.3.8 ==========
 
-# word = input("Please type in a word: ")
-# sent = word + " "
-# word2 = word
-# while True:
-#     word = input("Please type in a word: ")
-#     if word == "end" or word == word2:
-#         print(sent)
-#         break
-#     else:
-#        sent = sent + word + " "
-#        word2 = word 
+word = input("Please type in a word: ")
+sent = word + " "
+word2 = word
+while True:
+    word = input("Please type in a word: ")
+    if word == "end" or word == word2:
+        print(sent)
+        break
+    else:
+       sent = sent + word + " "
+       word2 = word 
 
 # ========== 2.3.9 ==========
 
